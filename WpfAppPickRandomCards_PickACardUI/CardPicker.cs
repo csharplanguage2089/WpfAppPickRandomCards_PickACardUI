96d13435cfa6c8d5c0dfc6823be21b4ae6951298
Andrey Yakovlev <andreyyakovlevindex@outlook.com>
2023-12-08 23:32:40 +0300

    Добавьте файлы проекта.

---
+++ WpfAppPickRandomCards_PickACardUI/CardPicker.cs
@@ -0,0 +1,55 @@
+﻿namespace WpfAppPickRandomCards_PickACardUI
+{
+    class CardPicker
+    {
+        static Random randomCoCo = new Random();
+
+        /// <summary>
+        /// Выбрать несколько карт
+        /// </summary>
+        /// <param name="numberOfCards"></param>
+        /// <returns>Массив строк с названием карт</returns>
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+
+            for (int i = 0; i < numberOfCards; i = i + 1)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+
+            // Команда return заставляет метод немедленно прервать выполнение и вернуться к команде, из которой он был вызван
+            return pickedCards;
+        }
+
+        // Случайная карта
+        private static string RandomSuit()
+        {
+            // Получить случайное число от 1 до 4, не включая 5
+            int valueCoCoDi = randomCoCo.Next(1, 5);
+
+            // Если 1, вернуть строку "Spades" Пики
+            if (valueCoCoDi == 1) return "Spades";
+            // Если 2, вернуть строку "Hearts" Сердца / Черви
+            if (valueCoCoDi == 2) return "Hearts";
+            // Если 3, вернуть строку "Clubs" Трефы / Клубы
+            if (valueCoCoDi == 3) return "Clubs";
+            // Если выполнение продолжается, вернуть строку "Diamonds" Бриллианты
+            return "Diamonds";
+        }
+
+        // Случайное значение
+        private static string RandomValue()
+        {
+            int valueCoCoDe = randomCoCo.Next(1, 14);
+
+            if (valueCoCoDe == 1) return "Ace";
+            if (valueCoCoDe == 11) return "Jack";
+            if (valueCoCoDe == 12) return "Queen";
+            if (valueCoCoDe == 13) return "King";
+            // valueCoCoDe является int, а метод RandomValue() был объявлен со строковым возвращаемым типом, поэтому valueCoCoDe необходимо преобразовать в строку
+            // Добавление .ToString() к любой переменной или значению преобразует их в строку
+            return valueCoCoDe.ToString();
+        }
+    }
+}
---
+++ WpfAppPickRandomCards_PickACardUI/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace WpfAppPickRandomCards_PickACardUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string[] pickerCards = CardPicker.PickSomeCards((int)numberOfCards.Value);
+            listOfCards.Items.Clear();
+            foreach (string myLocalCard in pickerCards)
+            {
+                listOfCards.Items.Add(myLocalCard);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            listOfCards.Items.Clear();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
